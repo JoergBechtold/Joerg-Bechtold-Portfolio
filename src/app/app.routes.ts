73ac4cbf9a3d2d56c9d0b26974d821a4a84795fc
Jörg Bechtold <joergbechtold@gmx.net>
2025-06-24 19:38:06 +0200

    set icons and background images and icons. set fonts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { AtfComponent } from './atf/atf.component';
+
+export const routes: Routes = [
+    { path: '', component: AtfComponent },
+];
