2d99898f28974033057015bc5b414f6876a53fff
Jörg Bechtold <joergbechtold@gmx.net>
2025-06-27 10:10:36 +0200

    implement first style in privacy and lagal and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { OverlayLandscapeComponent } from './overlay-landscape/overlay-landscape.component';
-import { AtfComponent } from './atf/atf.component';
-
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, OverlayLandscapeComponent, AtfComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, OverlayLandscapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from '../components-sections/about-me/about-me.component';
 import { MySkillsComponent } from '../components-sections/my-skills/my-skills.component';
 import { PortfolioComponent } from '../components-sections/portfolio/portfolio.component';
+import { AtfComponent } from "../atf/atf.component";
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [AboutMeComponent, MySkillsComponent, PortfolioComponent, AtfComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
