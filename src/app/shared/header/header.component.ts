c25270058cd4115fa7a11200f55a331c24e9b558
Jörg Bechtold <joergbechtold@gmx.net>
2025-06-27 11:28:47 +0200

    omplement i18n translate. it works by header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,18 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    TranslateModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  activeLanguage: string = 'EN';
+export class HeaderComponent implements OnInit {
+  activeLanguage: string = 'de';
 
-  setLanguage(language: string) {
-    this.activeLanguage = language;
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['de', 'en']);
+    translate.setDefaultLang('de');
   }
 
+  ngOnInit() {
+    const browserLang = this.translate.getBrowserLang();
+    const initialLang = browserLang && browserLang.match(/en|de/) ? browserLang : 'de';
+
+    this.translate.use(initialLang).subscribe(() => {
+      this.activeLanguage = initialLang;
+    });
+
+    this.translate.onLangChange.subscribe(event => {
+      this.activeLanguage = event.lang;
+    });
+  }
+
+  setLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
