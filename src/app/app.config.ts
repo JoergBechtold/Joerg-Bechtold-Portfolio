e33374e62679289c60dac6928ed3f0c7bc494cd7
Jörg Bechtold <joergbechtold@gmx.net>
2025-07-02 13:25:13 +0200

    app.config rebuilt so that there is less code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,56 +1,51 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import {
-  provideRouter,
-  withNavigationErrorHandler,
-  withInMemoryScrolling,
-  withRouterConfig
-} from '@angular/router';
-import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'; // <-- Korrigierter Importpfad
-import { routes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom, APP_INITIALIZER } from '@angular/core';
+import { provideRouter, withRouterConfig, withNavigationErrorHandler, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient, withFetch, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { APP_INITIALIZER } from '@angular/core';
+import { routes } from './app.routes';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http);
 }
 
-function initTranslations(translate: TranslateService) {
+
+const createRouterProvider = () => provideRouter(
+  routes,
+  withRouterConfig({ onSameUrlNavigation: 'reload' }),
+  withNavigationErrorHandler((error) => {
+    console.error('Router Navigation Error:', error);
+  }),
+  withInMemoryScrolling({ anchorScrolling: 'enabled' })
+);
+
+
+const createTranslationProviders = () => importProvidersFrom(
+  TranslateModule.forRoot({
+    loader: {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    },
+    defaultLanguage: 'de'
+  })
+);
+
+
+const initTranslations = (translate: TranslateService) => {
   return () => {
-    translate.addLangs(['de', 'en']);
-    // Optional: Setze die Standardsprache basierend auf dem Browser oder einem gespeicherten Wert
-    // translate.setDefaultLang('de');
-    // translate.use('de');
+    return translate.use(translate.getDefaultLang()).toPromise();
   };
-}
+};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes, // Deine definierten Routen
-      // Die Router-Konfiguration (ExtraOptions) wird jetzt mit withRouterConfig übergeben
-      withRouterConfig({
-        onSameUrlNavigation: 'reload'
-        // 'runGuardsAndResolvers' ist keine gültige Eigenschaft hier und wurde entfernt.
-        // Bei 'onSameUrlNavigation: 'reload'' werden Guards und Resolver in der Regel standardmäßig erneut ausgeführt.
-      }),
-      withNavigationErrorHandler((error) => {
-        console.error('Router Navigation Error:', error);
-      }),
-      withInMemoryScrolling({
-        anchorScrolling: 'enabled'
-      })
-    ),
+    createRouterProvider(),
     provideHttpClient(withFetch()),
-    importProvidersFrom(TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      },
-      defaultLanguage: 'de' // Setze die Standardsprache hier
-    })),
+    createTranslationProviders(),
     provideAnimations(),
     {
       provide: APP_INITIALIZER,
