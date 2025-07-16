b6e48bf672439f0f4273b90a4dab1706339b2c11
Jörg Bechtold <joergbechtold@gmx.net>
2025-07-16 13:27:50 +0200

    set content-box text and buttons

--- src/app/components-sections/portfolio/portfolio.component.ts
+++ src/app/components-sections/portfolio/portfolio.component.ts
@@ -3,29 +3,32 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateManagerService } from '../../services/translate/translate-manager.service';
 import { AnimateOnScrollDirective } from '../../shared/animation-on-scroll/animate-on-scroll.directive';
-import { PortfolioService, Project } from '../../services/portfolio/portfolio.service';
+import { PortfolioService, Project } from '../../services/portfolio/portfolio.service'; // <-- Project-Interface importieren!
 
 
 @Component({
     selector: 'app-portfolio',
     standalone: true,
-    imports: [CommonModule, TranslateModule],
+    imports: [CommonModule, TranslateModule], // <-- AnimateOnScrollDirective hinzufügen!
     templateUrl: './portfolio.component.html',
     styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent implements OnInit {
 
-    projectsService = inject(PortfolioService);
+    projectsService = inject(PortfolioService); // Korrekte Injektion deines Services
 
-    projects: Project[] = [];
-    activeLanguage: string = 'de';
+    projects: Project[] = []; // Deine Projektliste, korrekt typisiert
+    activeLanguage: string = 'de'; // Behält die aktive Sprache
 
     constructor(private translateManager: TranslateManagerService) { }
 
     ngOnInit(): void {
+        // Beobachtet Änderungen der aktiven Sprache, falls du darauf reagieren musst
         this.translateManager.activeLanguage$.subscribe(lang => {
             this.activeLanguage = lang;
         });
+
+        // Lädt die Projektdaten vom Service in die Komponente
         this.projects = this.projectsService.projectBox;
     }
 }
