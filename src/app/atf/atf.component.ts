2644ac43ebdb596cb3bf701ca5bee6b34d099d54
Jörg Bechtold <joergbechtold@gmx.net>
2025-07-17 08:34:53 +0200

    logic for atf button scroll to section contact built in

--- src/app/atf/atf.component.ts
+++ src/app/atf/atf.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { ContactNavAtfComponent } from "./contact-nav-atf/contact-nav-atf.component";
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateManagerService } from '../services/translate/translate-manager.service';
+import { AppRouteKeys } from '../app.routes';
 
 @Component({
   selector: 'app-atf',
+  standalone: true,
   imports: [CommonModule, ContactNavAtfComponent, TranslateModule],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss'
 })
 export class AtfComponent implements OnInit {
-
 
   activeLanguage: string = 'de';
 
@@ -21,5 +22,9 @@ export class AtfComponent implements OnInit {
     this.translateManager.activeLanguage$.subscribe(lang => {
       this.activeLanguage = lang;
     });
+  }
+
+  async navigateFromAtf(appRouteKey: keyof typeof AppRouteKeys): Promise<void> {
+    await this.translateManager.navigateToSection(appRouteKey);
   }
 }
