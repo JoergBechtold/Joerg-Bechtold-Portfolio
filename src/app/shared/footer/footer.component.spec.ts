6eda9568e4c39eb7c1f89dc9bab3502e1c4e44db
Jörg Bechtold <joergbechtold@gmx.net>
2025-07-18 11:56:35 +0200

    revised footer.spec.ts

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LINKComponent } from './footer.component';
+import { FooterComponent } from './footer.component';
 
-describe('LINKComponent', () => {
-  let component: LINKComponent;
-  let fixture: ComponentFixture<LINKComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LINKComponent]
+      imports: [FooterComponent]
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(LINKComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
